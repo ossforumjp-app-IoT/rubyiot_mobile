37c2b53e701bfddcf097cbdb60d04e86114f4d6f
dekigl <dekigl@gmail.com>
2015-10-26 00:48:34 +0900

    Gatewayデータ取得用処理追加

---
+++ app/models/Getway.rb
@@ -0,0 +1,35 @@
+class Getway
+  HTTP = 'http://'
+  SENSOR_URL = '/api/gateway'
+
+  # retriving data via api
+  def self.retrieve_data(block)
+    
+    retrieve_data_func = lambda do |login, block|
+      if login == false
+        block.call(nil)
+      end
+      
+      url = HTTP + $settings.server_address + SENSOR_URL
+      BW::HTTP.get(url, {cookie: $loginSession}) do |response|
+        if response.ok?
+          data = []
+          json = BW::JSON.parse(response.body.to_str)
+          json.each do |item|
+            gateway_id = item[0]
+            data << {
+              :id => gateway_id,
+              :hardware_uid => json[gateway_id]['hardware_uid'],
+              :name => json[gateway_id]['name'],
+            }
+          end
+          block.call(data)
+        else
+          block.call(nil)
+        end
+      end
+    end # lambda
+
+    ServerLogin.login(retrieve_data_func, block)
+  end
+end
